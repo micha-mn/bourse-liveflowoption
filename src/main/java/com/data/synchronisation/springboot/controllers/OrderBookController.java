fca842fd1cc81666d15ae41991ce041c7ce5486d
micha-mn <57522990+micha-mn@users.noreply.github.com>
2025-03-20 00:45:14 +0200

    Update OrderBookController.java

--- src/main/java/com/data/synchronisation/springboot/controllers/OrderBookController.java
+++ src/main/java/com/data/synchronisation/springboot/controllers/OrderBookController.java
@@ -1,44 +1,19 @@
 package com.data.synchronisation.springboot.controllers;
 
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
-import com.data.synchronisation.springboot.domain.entity.CrBTCHighLow;
-import com.data.synchronisation.springboot.domain.entity.CrBinanceHighLow;
-import com.data.synchronisation.springboot.domain.entity.CrEthereumHighLow;
-import com.data.synchronisation.springboot.domain.entity.CrShibaHighLow;
-import com.data.synchronisation.springboot.domain.entity.CrSolanaHighLow;
-import com.data.synchronisation.springboot.domain.entity.CrXrpHighLow;
-import com.data.synchronisation.springboot.dto.CurrencyDTO;
-import com.data.synchronisation.springboot.dto.DataDTO;
 import com.data.synchronisation.springboot.dto.GraphDataReqDTO;
-import com.data.synchronisation.springboot.dto.GraphFulllResponseDTO;
-import com.data.synchronisation.springboot.dto.GraphGeneralResponseDTO;
-import com.data.synchronisation.springboot.dto.GraphResponseDTO;
 import com.data.synchronisation.springboot.dto.GraphResponseProjection;
 import com.data.synchronisation.springboot.dto.OrderBookResponseDTO;
-import com.data.synchronisation.springboot.dto.SupportResistantPointsDTO;
-import com.data.synchronisation.springboot.dto.TradeHistoryResDTO;
-import com.data.synchronisation.springboot.dto.TradeReqDTO;
-import com.data.synchronisation.springboot.dto.TradeResponseDTO;
 import com.data.synchronisation.springboot.dto.orderBook.OrderBookDataDTO;
-import com.data.synchronisation.springboot.service.CryptoAnalyseHighLowService;
-import com.data.synchronisation.springboot.service.CryptoAnalyseService;
 import com.data.synchronisation.springboot.service.OrderBookService;
-
 
 
 @RestController
@@ -67,7 +42,7 @@ public class OrderBookController {
 		 return orderBookService.getOrderBookByBidAsk(req);
 	}
 	
-	@GetMapping(value = "/get-order-book-data")
+	@PostMapping(value = "/get-order-book-data")
 	public OrderBookDataDTO getOrderBookData(@RequestBody GraphDataReqDTO req) {
 		// return null;
 		 return orderBookService.getOrderBookData(req);
